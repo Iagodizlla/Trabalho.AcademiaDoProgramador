468d8a86c0b66ee98cbbaeb230bebaa6a3769aa9
Iagodizlla <iagoschlemper@gmail.com>
2025-03-25 19:00:20 -0300

    Estruturacao basica e cabecalho

--- TrabalhoA.P.ConsoleApp/Program.cs
+++ TrabalhoA.P.ConsoleApp/Program.cs
@@ -4,34 +4,31 @@
     {
         static void Main(string[] args)
         {
-            int n;
-            while (true) {
-                Console.Clear();
-                Console.Write("Digite um numero Impar: ");
-                n = int.Parse(Console.ReadLine()!);
+            int n = VerificarNumero();
 
-                if (n % 2 == 0)
-                {
-                    Console.WriteLine("Numero nao pode ser Par");
-                }
-                else if(n < 1)
-                {
-                    Console.WriteLine("Numero nao pode ser negativo");
-                }
-                else
-                {
-                    break;
-                }
-                Console.ReadLine();
-            }
+            DiamanteDesenho(n);
+            
+            Console.ReadLine();
+        }
+        static void Cabecalho()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("| *     *          *         *|");
+            Console.WriteLine("|      *  DIAMANTE DE X      *|");
+            Console.WriteLine("|   *            *  *       * |");
+            Console.WriteLine("-------------------------------\n\n");
+        }
+        static void DiamanteDesenho(int n)
+        {
             int branco = n / 2 + 5, X = 1;
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < branco;j++)
+                for (int j = 1; j <= branco; j++)
                 {
                     Console.Write(" ");
                 }
-                for(int j = 0; j < X;j++)
+                for (int j = 1; j <= X; j++)
                 {
                     Console.Write("X");
                 }
@@ -39,15 +36,38 @@
                 if (i < n / 2)
                 {
                     branco--;
-                    X+=2;
+                    X += 2;
                 }
                 else
                 {
                     branco++;
-                    X-=2;
+                    X -= 2;
                 }
             }
-            Console.ReadLine();
+        }
+        static int VerificarNumero()
+        {
+            int n;
+            while (true)
+            {
+                Cabecalho();
+                Console.Write("Digite um numero Impar: ");
+                n = int.Parse(Console.ReadLine()!);
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("Numero nao pode ser Par");
+                }
+                else if (n < 1)
+                {
+                    Console.WriteLine("Numero nao pode ser negativo");
+                }
+                else
+                {
+                    return n;
+                }
+                Console.ReadLine();
+            }
+            
         }
     }
 }
