ce2427760ab905e940adf5dbe3fe183fbab24396
Iagodizlla <iagoschlemper@gmail.com>
2025-03-25 17:10:56 -0300

    Implementado codificacao basica

--- TrabalhoA.P.ConsoleApp/Program.cs
+++ TrabalhoA.P.ConsoleApp/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n;
+            while (true) {
+                Console.Clear();
+                Console.Write("Digite um numero Impar: ");
+                n = int.Parse(Console.ReadLine()!);
+
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("Numero nao pode ser Par");
+                }
+                else if(n < 1)
+                {
+                    Console.WriteLine("Numero nao pode ser negativo");
+                }
+                else
+                {
+                    break;
+                }
+                Console.ReadLine();
+            }
+            int branco = n / 2 + 5, X = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < branco;j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < X;j++)
+                {
+                    Console.Write("X");
+                }
+                Console.Write("\n");
+                if (i < n / 2)
+                {
+                    branco--;
+                    X+=2;
+                }
+                else
+                {
+                    branco++;
+                    X-=2;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
